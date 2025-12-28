d690db21e63e5701c820ed0b25b2b5c67836ea91
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2025-12-28 12:53:08 +0530

    Add files via upload
    This file contains inner join and left join

---
+++ types of joins.sql
@@ -0,0 +1,38 @@
+SELECT * FROM mark;
+
+SELECT * FROM student;
+
+-- haha
+/*
+this query is for inner join 
+
+*/
+
+-- inner join
+SELECT student.name,mark.score
+FROM student
+INNER JOIN mark
+ON student.id = mark.id;
+
+
+
+INSERT INTO student(id,name) 
+VALUES 
+(5,'Goldy');
+
+SELECT * FROM student;
+
+
+
+-- left join
+
+SELECT student.name,mark.score 
+FROM student
+LEFT JOIN mark
+ON student.id = mark.id;
+
+
+-- SELECT mark.score,student.name
+-- FROM mark
+-- LEFT JOIN student
+-- ON mark.id = student.id;
